6ef6407fa8e64e4d5841ce22846b61730dd48307
Maksymilian Gajowniczek <maksymilian.gajowniczek@kcl.ac.uk>
2017-03-21 22:27:01 +0000

    Refactor potControl.ino

--- potControl/potControl.ino
+++ potControl/potControl.ino
@@ -7,10 +7,10 @@
 #include <Servo.h> 
 
 //create servo objects (maximum of 8 can be used)
-Servo baseservo;
-Servo liftservo;
-Servo pushservo;
-Servo clawservo;
+Servo baseServo;
+Servo liftServo;
+Servo pushServo;
+Servo clawServo;
 
 // Analog pins to connect potentiometers to.
 const int baseInPin = A0; 
@@ -18,49 +18,43 @@ const int liftInPin = A1;
 const int pushInPin = A2;
 const int clawInPin = A3;
 
-// Variables to read analog voltages (0-255)
-int baseValue = 0;
-int liftValue = 0;
-int pushValue = 0;
-int clawValue = 0;
 // Variables to map to 0 degrees to 180 degree range
-int baseOutput = 0;
-int liftOutput = 0;
-int pushOutput = 0;
-int clawOutput = 0;
+int baseDegrees = 0;
+int liftDegrees = 0;
+int pushDegrees = 0;
+int clawDegrees = 0;
 
 
 void setup()
 {
-  baseservo.attach(3);
-  liftservo.attach(5);
-  pushservo.attach(6);
-  clawservo.attach(9);
+  baseServo.attach(3);
+  liftServo.attach(5);
+  pushServo.attach(6);
+  clawServo.attach(9);
   Serial.begin(9600);
 } 
 
-
 void loop()
 {
-// read the analog in value:
-  baseValue = analogRead(baseInPin);
-  liftValue = analogRead(liftInPin);
-  pushValue = analogRead(pushInPin);
-  clawValue = analogRead(clawInPin);
-  // map it to the range of the analog out:
-  baseOutput = map(baseValue, 0, 1023, 0, 180);
-  liftOutput = map(liftValue, 0, 1023, 0, 180);
-  pushOutput = map(pushValue, 0, 1023, 0, 180);
-  clawOutput = map(clawValue, 0, 1023, 0, 180);
-
-  Serial.print("outputs = ");
-  Serial.print(baseOutput); Serial.print("\t ");
-  Serial.print(liftOutput); Serial.print("\t ");
-  Serial.print(pushOutput); Serial.print("\t ");
-  Serial.println(clawOutput);
-  baseservo.write(baseOutput);
-  liftservo.write(liftOutput);
-  pushservo.write(pushOutput);
-  clawservo.write(clawOutput);
+  // read analog values and map it to the range of 0-180 degrees
+  // TODO: why is it 1023, if arduino's precision is 8bit=255?
+  baseDegrees = map(analogRead(baseInPin), 0, 1023, 0, 180);
+  liftDegrees = map(analogRead(liftInPin), 0, 1023, 0, 180);
+  pushDegrees = map(analogRead(pushInPin), 0, 1023, 0, 180);
+  clawDegrees = map(analogRead(clawInPin), 0, 1023, 0, 180);
+  // serialPrintServoValues(baseDegrees, liftDegrees, pushDegrees, clawDegrees);
+  baseServo.write(baseDegrees);
+  liftServo.write(liftDegrees);
+  pushServo.write(pushDegrees);
+  clawServo.write(clawDegrees);
   delay(15);   // waits 15ms for the servo to reach the position 
+}
+
+void serialPrintServoValues(int base, int lift, int push, int claw)
+{
+  Serial.print("outputs = ");
+  Serial.print(base); Serial.print("\t ");
+  Serial.print(lift); Serial.print("\t ");
+  Serial.print(push); Serial.print("\t ");
+  Serial.println(claw);
 }
